19178cb3733ed6e260ffc174d066a3764cc4ea1b
sanghun <vitash1215@gmail.com>
2025-09-27 20:07:40 +0900

    refactor: 지도 컴포넌트 모듈화 및 코드 개선
    - MapComponent를 MapLegend, MapInstructions로 분리하여 모듈화
    - constants.ts로 설정값 중앙 관리 및 환경변수 처리
    - 코드 가독성 향상을 위한 상세한 주석 추가
    - 호버 효과 및 줌 기능 최적화
    - 불필요한 OpenLayers 컨트롤 제거
    - 전체 화면 지도 표시 및 UI 개선

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,15 +1,25 @@
-import React from 'react'
-import MapComponent from './components/MapComponent'
-import './App.css'
+/**
+ * 메인 애플리케이션 컴포넌트
+ * 지오스페이셜 웹 애플리케이션의 루트 컴포넌트
+ */
+import React from "react";
+import MapComponent from "./components/MapComponent";
+import "./App.css";
 
 function App() {
   return (
-    <div style={{ padding: '20px' }}>
-      <h1>지오스페이셜 웹 애플리케이션</h1>
-      <p>React + OpenLayers + GeoServer + PostGIS</p>
-      <MapComponent />
+    <div className="app">
+      {/* 애플리케이션 헤더 */}
+      <header className="app-header">
+        <h1>지오스페이셜 웹 애플리케이션</h1>
+        <p>React + OpenLayers + GeoServer + PostGIS</p>
+      </header>
+      {/* 메인 지도 컴포넌트 */}
+      <main className="app-main">
+        <MapComponent />
+      </main>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
